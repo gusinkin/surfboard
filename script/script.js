cb3c5f5b46bd99e86a16d892685239c5643fe32a
Dmitry Gusinkin <d.gusinkin@gmail.com>
2022-03-21 00:34:26 +0300

    week 3

---
+++ script/script.js
@@ -0,0 +1,12 @@
+const openButton = document.querySelector('.hamburger')
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const closeButton = document.querySelector('.hamburger-menu__close')
+
+openButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  hamburgerMenu.classList.add('active')
+})
+closeButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  hamburgerMenu.classList.remove('active')
+})
